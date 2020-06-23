bf00d21889e41c679cce353b1796ce673d2c4f86
Timur Aliev <alievtm@gmail.com>
2020-06-23 19:55:22 +0300

    lesson 13

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="Nav">
+            <ul>
+            <li>Профиль</li>
+            <li>Новости</li>
+            <li>Сообщения</li>
+            <li>Музыка</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="Content">
+            <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" />
+            MainContent
+        </div>
+    );
+}
+
+export default Profile;
