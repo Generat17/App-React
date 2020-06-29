3899f1b69c6bc331a374a9da0e073b9957b47238
Timur Aliev <alievtm@gmail.com>
2020-06-29 03:14:59 +0300

    lesson 37

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import * as serviceWorker from './serviceWorker';
-import state, {newPost, subscribe, updateNewPosText} from './redux/state';
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
+import './index.css';
+import store from "./redux/state";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App appState={state} newPost={newPost} updateNewPostText={updateNewPosText}/>
+            <App
+                appState={state}
+                newPost={store.newPost.bind(store)}
+                updateNewPostText={store.updateNewPosText.bind(store)}/>
         </React.StrictMode>, document.getElementById('root')
     );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,58 +1,60 @@
-let state = {
-    messagePage : {
-        messagesData: [
-            {id: 0, message: 'Hi, it s me!'},
-            {id: 1, message: 'I have to go'},
-            {id: 2, message: 'see u'},
-            {id: 3, message: 'bb'},
-            {id: 4, message: 'Freddy Cruger'},
-            {id: 5, message: '354643'},
-            {id: 6, message: 'lift up to reciver'}
-        ],
-        dialogsData: [
-            {id: 0, name: 'Boris Prank'},
-            {id: 1, name: 'AlexUnder NewSky'},
-            {id: 2, name: 'Aliev Timur'},
-            {id: 3, name: 'Alexunder Adamov'},
-            {id: 4, name: 'Alieva Ludmila'},
-            {id: 5, name: 'Goncharova Tatiana'}
-        ]
+let store = {
+    _state: {
+        messagePage: {
+            messagesData: [
+                {id: 0, message: 'Hi, it s me!'},
+                {id: 1, message: 'I have to go'},
+                {id: 2, message: 'see u'},
+                {id: 3, message: 'bb'},
+                {id: 4, message: 'Freddy Cruger'},
+                {id: 5, message: '354643'},
+                {id: 6, message: 'lift up to reciver'}
+            ],
+            dialogsData: [
+                {id: 0, name: 'Boris Prank'},
+                {id: 1, name: 'AlexUnder NewSky'},
+                {id: 2, name: 'Aliev Timur'},
+                {id: 3, name: 'Alexunder Adamov'},
+                {id: 4, name: 'Alieva Ludmila'},
+                {id: 5, name: 'Goncharova Tatiana'}
+            ]
+        },
+        profilePage: {
+            postData: [
+                {id: 0, message: 'Hello! My name\'s Aliev Timur :D'},
+                {id: 1, message: 'What makes every.'},
+                {id: 2, message: 'American a typical one is a desire!'},
+                {id: 3, message: 'to get a well-paid job that.'},
+                {id: 4, message: 'Some companies now!'}
+            ],
+            newPostText: ''
+        }
     },
-    profilePage : {
-        postData: [
-            {id: 0, message: 'Hello! My name\'s Aliev Timur :D'},
-            {id: 1, message: 'What makes every.'},
-            {id: 2, message: 'American a typical one is a desire!'},
-            {id: 3, message: 'to get a well-paid job that.'},
-            {id: 4, message: 'Some companies now!'}
-        ],
-        newPostText: ''
-    }
-};
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('hello');
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+    newPost(newMessage) {
+        let currentPost = {
+            id: 5,
+            message: newMessage
+        }
 
-let rerenderEntireTree = () => {
-    console.log('hello');
-}
-
-export let subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export let newPost = (newMessage) => {
-    debugger;
-    let currentPost = {
-        id : 5,
-        message : newMessage
+        this._state.profilePage.postData.push(currentPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPosText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
     }
 
-    state.profilePage.postData.push(currentPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
 }
 
-export let updateNewPosText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
+window.store = store;
